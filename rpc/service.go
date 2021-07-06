9ee3613e38624508377083aa6d0b006cb714a42a
zulong210220 <zulong210220@gmail.com>
2021-07-06 17:49:39 +0800

    add service

---
+++ rpc/service.go
@@ -0,0 +1,122 @@
+package rpc
+
+/*
+ * Author : lijinya
+ * Email : yajin160305@gmail.com
+ * File : service.go
+ * CreateDate : 2021-07-06 16:59:18
+ * */
+
+import (
+	"go/ast"
+	"reflect"
+	"sync/atomic"
+
+	"github.com/sirupsen/logrus"
+)
+
+type methodType struct {
+	method    reflect.Method
+	ArgType   reflect.Type
+	ReplyType reflect.Type
+	numCalls  uint64
+}
+
+func (m *methodType) NumCalls() uint64 {
+	return atomic.LoadUint64(&m.numCalls)
+}
+
+func (m *methodType) newArgv() reflect.Value {
+	var argv reflect.Value
+
+	if m.ArgType.Kind() == reflect.Ptr {
+		argv = reflect.New(m.ArgType.Elem())
+	} else {
+		argv = reflect.New(m.ArgType).Elem()
+	}
+	return argv
+}
+
+func (m *methodType) newReplyv() reflect.Value {
+	replyv := reflect.New(m.ReplyType.Elem())
+
+	switch m.ReplyType.Elem().Kind() {
+	case reflect.Map:
+		replyv.Elem().Set(reflect.MakeMap(m.ReplyType.Elem()))
+	case reflect.Slice:
+		replyv.Elem().Set(reflect.MakeSlice(m.ReplyType.Elem(), 0, 0))
+	}
+	return replyv
+}
+
+type service struct {
+	name   string
+	typ    reflect.Type
+	rcvr   reflect.Value
+	method map[string]*methodType
+}
+
+func newService(rcvr interface{}) *service {
+	fun := "newService"
+	s := new(service)
+	s.rcvr = reflect.ValueOf(rcvr)
+	s.name = reflect.Indirect(s.rcvr).Type().Name()
+	s.typ = reflect.TypeOf(rcvr)
+
+	if !ast.IsExported(s.name) {
+		logrus.Fatalf("%s rpc server : %s is not a valid service name", fun, s.name)
+	}
+
+	s.registerMethods()
+	return s
+}
+
+func isExportedOrBuiltinType(t reflect.Type) bool {
+	return ast.IsExported(t.Name()) || t.PkgPath() == ""
+}
+
+func (s *service) registerMethods() {
+	s.method = make(map[string]*methodType)
+
+	for i := 0; i < s.typ.NumMethod(); i++ {
+		method := s.typ.Method(i)
+		mType := method.Type
+		if mType.NumIn() != 3 || mType.NumOut() != 1 {
+			continue
+		}
+
+		if mType.Out(0) != reflect.TypeOf((*error)(nil)).Elem() {
+			continue
+		}
+
+		argType, replyType := mType.In(1), mType.In(2)
+		if !isExportedOrBuiltinType(argType) || !isExportedOrBuiltinType(replyType) {
+			continue
+		}
+
+		s.method[method.Name] = &methodType{
+			method:    method,
+			ArgType:   argType,
+			ReplyType: replyType,
+		}
+	}
+}
+
+func (s *service) call(m *methodType, argv, replyv reflect.Value) error {
+	atomic.AddUint64(&m.numCalls, 1)
+
+	f := m.method.Func
+
+	retVal := f.Call([]reflect.Value{
+		s.rcvr, argv, replyv,
+	})
+
+	// TODO check slice len
+	if errInter := retVal[0].Interface(); errInter != nil {
+		return errInter.(error)
+	}
+
+	return nil
+}
+
+/* vim: set tabstop=4 set shiftwidth=4 */
---
+++ rpc/service_test.go
@@ -0,0 +1,71 @@
+package rpc
+
+/*
+ * Author : lijinya
+ * Email : yajin160305@gmail.com
+ * File : service_test.go
+ * CreateDate : 2021-07-06 17:32:20
+ * */
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
+
+type Foo int
+
+type Args struct {
+	Num1, Num2 int
+}
+
+func (f Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
+func (f Foo) sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
+func TestNS(t *testing.T) {
+	var f Foo
+	s := newService(&f)
+
+	if len(s.method) != 1 {
+		t.Fatalf("wrong service methhod expect 1, but got %d", len(s.method))
+	}
+
+	mType := s.method["Sum"]
+	if mType == nil {
+		t.Fatal("wrong method, Sum should't nil")
+	}
+	logrus.Info(mType)
+}
+
+func TestFoo(t *testing.T) {
+	var f Foo
+	s := newService(&f)
+	mType := s.method["Sum"]
+
+	argv := mType.newArgv()
+	replyv := mType.newReplyv()
+
+	argv.Set(reflect.ValueOf(Args{Num1: 11, Num2: 22}))
+	err := s.call(mType, argv, replyv)
+
+	if !(err == nil && *replyv.Interface().(*int) == 33 && mType.NumCalls() == 1) {
+		t.Fatalf("failed to call Foo.Sum")
+	}
+
+	argv.Set(reflect.ValueOf(Args{Num1: 11, Num2: 12}))
+	err = s.call(mType, argv, replyv)
+
+	if !(err == nil && *replyv.Interface().(*int) == 23 && mType.NumCalls() == 2) {
+		t.Fatalf("failed to call Foo.Sum")
+	}
+}
+
+/* vim: set tabstop=4 set shiftwidth=4 */
